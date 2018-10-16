c22da9203dc6af78142e50067add05c9660c0eb0
AlmazSamatov <samatov.almaz@yandex.ru>
2018-10-16 15:28:34 +0300

    implemented login fully, only need to change API link

--- app/src/main/java/company/solnyshko/mobileapp/Login/LoginActivity.kt
+++ app/src/main/java/company/solnyshko/mobileapp/Login/LoginActivity.kt
@@ -1,28 +1,44 @@
 package company.solnyshko.mobileapp.Login
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
+import company.solnyshko.mobileapp.MainActivity
 import company.solnyshko.mobileapp.R
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity(), LoginView {
     override fun turnOffProgressBar() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        progress_bar.visibility = GONE
+        editText.visibility = VISIBLE
+        editText2.visibility = VISIBLE
+        button2.visibility = VISIBLE
     }
 
     override fun turnOnProgressBar() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        progress_bar.visibility = VISIBLE
+        editText.visibility = GONE
+        editText2.visibility = GONE
+        button2.visibility = GONE
     }
 
-    override fun showError(s: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun showError(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 
     override fun launchMainActivity(userID: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val intent = Intent(baseContext, MainActivity::class.java)
+        intent.putExtra("userID", userID)
+        startActivity(intent)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+
     }
 }
