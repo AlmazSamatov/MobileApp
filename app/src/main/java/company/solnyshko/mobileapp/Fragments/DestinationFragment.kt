0bd5418ba32dbf8cbedd69a039a59b7e10906523
AlmazSamatov <samatov.almaz@yandex.ru>
2018-10-18 16:02:10 +0300

    implemented

--- app/src/main/java/company/solnyshko/mobileapp/Fragments/DestinationFragment.kt
+++ app/src/main/java/company/solnyshko/mobileapp/Fragments/DestinationFragment.kt
@@ -1,10 +1,12 @@
 package company.solnyshko.mobileapp.Fragments
 
 import android.app.Fragment
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import company.solnyshko.mobileapp.Map.MapsActivity
 import company.solnyshko.mobileapp.R.layout.destination_activity
 import kotlinx.android.synthetic.main.destination_activity.*
 
@@ -16,7 +18,8 @@ class DestinationFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         show_in_map.setOnClickListener {
-
+            val intent = Intent(activity, MapsActivity::class.java)
+            startActivity(intent)
         }
     }
 
