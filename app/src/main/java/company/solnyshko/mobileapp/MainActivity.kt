abf6f270d3b861cd56f4f1e3cbbb6d9c39f8f783
Amir Nazyrov <a.nazyroff@mail.ru>
2018-10-18 13:11:15 +0300

    Add MapActivity with some functionality
    I added new Activity layout with Google Map where are you can see 2 points

--- app/src/main/java/company/solnyshko/mobileapp/MainActivity.kt
+++ app/src/main/java/company/solnyshko/mobileapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import company.solnyshko.mobileapp.Fragments.InfoFragment
+import company.solnyshko.mobileapp.Map.MapsActivity
 import kotlinx.android.synthetic.main.activity_info.*
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +34,10 @@ class MainActivity : AppCompatActivity() {
                     fragmentManager.replace(R.id.fragment, InfoFragment())
                     fragmentManager.commit()
                 }
-                //R.id.action_map ->
+                R.id.action_map -> {
+                    val intent = Intent(this, MapsActivity::class.java)
+                    startActivity(intent)
+                }
                 //R.id.action_chat ->
                 R.id.action_call -> {
                     val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:89991564759"))
